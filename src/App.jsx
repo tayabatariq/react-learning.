dc5a0615feecfe2f7ad486baf72010e5ff4be51b
tayabatariq <168851346+tayabatariq@users.noreply.github.com>
2024-12-22 22:03:53 +0500

    initial changing

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold bg-black text-red-600 underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
