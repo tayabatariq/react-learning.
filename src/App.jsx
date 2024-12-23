46610c5463b4a0b2028563a0d39982c4beeb0672
tayabatariq <168851346+tayabatariq@users.noreply.github.com>
2024-12-23 20:02:13 +0500

    basics concepts

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold bg-black text-red-600 underline">
-    Hello world!
-  </h1>
+    <>
+    <div className='bg-zinc-900 p-7  w-full h-screen flex items-center justify-center gap-4 flex-col '>
+    <h1 className="text-6xl text-center font-bold text-white  underline">
+      Welcome to My React Jorney 
+      </h1>
+      <p className='text-white text-xl  '>Aaj maine React sikhna shuru kiya hai! Yeh mera pehla React project hai, 
+      jismein maine simple concepts explore kiye hain. 😊 
+      </p>
+      <button onClick={()=> {
+        alert("I'm happy to Learn React ")
+      }} className='text-white cursor-pointer hover:bg-zinc-100 hover:text-black bg-zinc-500 p-2 rounded-lg '> Click Here to Cheer Me Up! 🎉</button>
+    </div>
+  </>
+
   )
 }
 
