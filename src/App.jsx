0a3a22635111725622c64613d006d818ef7e5e35
tayabatariq <168851346+tayabatariq@users.noreply.github.com>
2024-12-24 16:57:13 +0500

    inital chnaging

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,10 @@ function App() {
       <p className='text-white text-xl  '>Aaj maine React sikhna shuru kiya hai! Yeh mera pehla React project hai, 
       jismein maine simple concepts explore kiye hain. 😊 
       </p>
+      
       <button onClick={()=> {
         alert("I'm happy to Learn React ")
-      }} className='text-white cursor-pointer hover:bg-zinc-100 hover:text-black bg-zinc-500 p-2 rounded-lg '> Click Here to Cheer Me Up! 🎉</button>
+      }} className='text-white cursor-pointer hover:bg-zinc-100 hover:text-black bg-zinc-500 hover:rounded-md p-2 rounded-lg '> Click Here to Cheer Me Up! 🎉</button>
     </div>
   </>
 
@@ -20,3 +21,8 @@ function App() {
 }
 
 export default App
+
+
+
+
+
---
+++ src/Myapp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyApp(){
+    return (
+    <>
+    <h1 className='w-full h-screen bg-zinc-100 text-black '>This is my App custome reaact app wesbite</h1>
+    </>
+    )
+}
+
+export default MyApp
